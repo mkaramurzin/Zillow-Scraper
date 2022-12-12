8d2f6a853a548a11831e90ca5b173fbd032eed85
unknown <mkaramurzin02@gmail.com>
2022-12-12 14:36:13 -0800

    file writing and index page implemented

--- scraper/views.py
+++ scraper/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+from django.views.decorators.csrf import csrf_exempt
+from csv import writer
+from django.http import HttpResponse
+import os
 
-# Create your views here.
-
+@csrf_exempt
 def index(request):
-    return render(request, "scraper/index.html")
+    if request.method == 'GET':
+        return render(request, "scraper/index.html")
+    
+
+    url = request.POST['url']
+    file = 'hosuing.csv'
+    with open(file, 'w', encoding='utf-8', newline='') as f:
+        w = writer(f)
+        header = ['Title', 'Location', 'Price']
+        w.writerow(header)
+
+    with open(file, 'rt', encoding='utf-8', newline='') as f:
+        data = f.read()
+
+    os.remove(file)
+    response = HttpResponse(data)
+    response['Content-Disposition'] = 'attachment; filename="housing.csv"'
+    return response
